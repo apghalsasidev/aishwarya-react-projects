75411ab07ca6e72b6a38d9c3568f31e14f72c104
apghalsasidev <a.p.ghalsasi@gmail.com>
2026-04-26 20:45:05 +0530

    initial commit for my-todo-app

--- my-todo-app/App.js
+++ my-todo-app/App.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import TodoApp from './TodoApp';
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 
